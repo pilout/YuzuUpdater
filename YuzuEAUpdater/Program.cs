84d25ee31329a31e74451e64b221277192ea7df6
clem <tnemelc12@hotmail.fr>
2023-05-14 21:31:38 +0200

    Ajoutez des fichiers projet.

---
+++ YuzuEAUpdater/Program.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Net.WebRequestMethods;
+
+namespace YuzuEAUpdater
+{
+    internal class Program
+    {
+        private static List<Release> releases = new List<Release>();
+        private static List<PR> prList = new List<PR>();
+
+        private static  string currentVersion = null;
+        private static Release myCurrentRelease = null;
+
+
+        static void Main(string[] args)
+        {
+            getCurrentVersion();
+            checkVersion();
+            Console.WriteLine("Starting Yuzu...");
+            Process.Start("yuzu.exe");
+        }
+
+        static void getCurrentVersion()
+        {
+            if(System.IO.File.Exists("yuzu.exe"))
+            {
+                StreamReader reader = new StreamReader("yuzu.exe");
+                currentVersion = reader.ReadToEnd();
+                reader.Close();
+                var index = currentVersion.IndexOf("yuzu Early Access");
+                currentVersion = currentVersion.Substring(index - 20, 20).Replace("\0", "").Replace("\00", "");
+                if(currentVersion.StartsWith("0"))
+                    currentVersion = currentVersion.Substring(1);
+
+                currentVersion = "EA-" + currentVersion;
+                Console.WriteLine("Yuzu EA version found : " + currentVersion);
+            }
+
+        }
+
+        public static void checkVersion()
+        {
+
+            Console.WriteLine("Retrives releases from github");
+            WebClient client = new WebClient();
+            String src = client.DownloadString("https://github.com/pineappleEA/pineapple-src/releases/");
+
+            string[] releaseVersions = src.Split(new String[] { "h2 class=\"sr-only\"" }, StringSplitOptions.RemoveEmptyEntries).Skip(1).ToArray(); 
+            releaseVersions = releaseVersions.Take(releaseVersions.Length - 1).ToArray();
+
+            for(int i = 0;  i < releaseVersions.Length; i++)
+            {
+                releases.Add(new Release(releaseVersions[i]));
+            }
+
+            if(currentVersion == null)
+            {
+                Console.WriteLine("No version found, but find latest version");
+
+      
+            }
+            else
+            {
+                myCurrentRelease = releases.Where(x => x.version == currentVersion).FirstOrDefault();
+                if (myCurrentRelease != null)
+                {
+                    if (myCurrentRelease != releases.FirstOrDefault())
+                    {
+                        Console.WriteLine("Retrieve PRs from github");
+                        for (var p = 0; p < 4; p++)
+                        {
+                            src = client.DownloadString("https://github.com/yuzu-emu/yuzu/issues?page=" + p + "&q=sort%3Acreated-desc");
+
+                            string[] prs = src.Split(new String[] { "<div class=\"flex-auto min-width-0 p-2 pr-3 pr-md-2\">" }, StringSplitOptions.RemoveEmptyEntries).Skip(1).ToArray();
+                            prs = prs.Take(prs.Length - 1).ToArray();
+
+                            for (int i = 0; i < prs.Length; i++)
+                            {
+                                var pr = new PR(prs[i]);
+                                //CHECK IF UNIQUE BECAUSE BUG IDK WHY
+                                if(prList.FirstOrDefault(x => x.idIssue == pr.idIssue) == null)
+                                 prList.Add(pr);
+                            }
+                        }
+
+
+                        string changeLog = getChangeLog();
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("Yuzu is up to date");
+                        return;
+                    }
+                }
+                Console.WriteLine("New Version found , pass from " + currentVersion + " to " + releases[0].version + "\n" + getChangeLog());
+
+            }
+
+            Console.WriteLine("Do you want to download it ? (y/n)");
+            string answer = Console.ReadLine();
+            if (answer == "y")
+            {
+                downloadLastedVersion();
+            }
+            else
+            {
+                Console.WriteLine("Ok, bye");
+                System.Environment.Exit(0);
+            }
+
+        }
+
+        private static  void  downloadLastedVersion()
+        {
+            Console.WriteLine("Downloading latest version");
+            WebClient client = new WebClient();
+            client.DownloadFile(releases[0].downloadUrl, "YuzuEA.zip");
+            ZipArchive zip = ZipFile.OpenRead("YuzuEA.zip");
+            zip.ExtractToDirectory(System.Environment.CurrentDirectory,true);
+            zip.Dispose();
+            Console.WriteLine("Remove zip file");
+            System.IO.File.Delete("YuzuEA.zip");
+            string[] files = Directory.GetFiles(System.Environment.CurrentDirectory + "/yuzu-windows-msvc-early-access");
+            Console.WriteLine("Move files and directory to root directory");
+            Utils.DirectoryCopyAndDelete(System.Environment.CurrentDirectory + "/yuzu-windows-msvc-early-access", System.Environment.CurrentDirectory);
+        }
+
+        private static string getChangeLog()
+        {
+            string changeLog = "";
+            List<PR> prs = prList.Where(x => x.releaseDate > myCurrentRelease.releaseDate  && x.label.Contains("merge")).ToList();
+
+            foreach(PR pr in prs)
+            {
+                changeLog += pr.idIssue + " - " + pr.description + " - " + pr.label + " - " + pr.releaseDate + "\n";
+            }
+            return changeLog;
+        }
+    }
+
+
+    public class Release
+    {
+        public Release(string releaseVersion)
+        {
+
+            this.version = Regex.Match(releaseVersion, @""">(.*)</h2>").Groups[1].Value;
+            this.downloadUrl = @"https://github.com/pineappleEA/pineapple-src/releases/download/"+ version + "/Windows-Yuzu-"+ version + ".zip";
+            this.releaseDate = DateTime.Parse(Regex.Match(releaseVersion, @"datetime=""(.*)"">").Groups[1].Value);
+
+        }
+
+        public string version;
+        public string downloadUrl;
+        public DateTime releaseDate;
+    }
+
+    public class PR
+    {
+        public PR(string prVersion)
+        {
+             idIssue = Regex.Match(prVersion, @"<a id=""issue_(\d*)_link").Groups[1].Value;
+             description = Regex.Match(prVersion, @"<a id=""issue_\d*_link"".*>(.*)</a>").Groups[1].Value;
+             releaseDate = DateTime.Parse(Regex.Match(prVersion, @"datetime=""(.*)Z""").Groups[1].Value + "Z");
+
+            var labels = Regex.Matches(prVersion, @"data-name=""(.*)"" style=");
+            foreach(Match label in labels)
+            {
+                this.label += label.Groups[1].Value + " ";
+            }
+        }
+
+        public string idIssue;
+        public string description;
+        public DateTime releaseDate;
+        public string label = "";
+
+    }
+}
---
+++ YuzuEAUpdater/ZipExtension.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Compression;
+
+namespace YuzuEAUpdater
+{
+        public static class Utils
+        {
+            public static void ExtractToDirectory(this ZipArchive archive, string destinationDirectoryName, bool overwrite)
+            {
+             
+                if (!overwrite)
+                {
+                    archive.ExtractToDirectory(destinationDirectoryName);
+                    return;
+                }
+
+                DirectoryInfo di = Directory.CreateDirectory(destinationDirectoryName);
+                string destinationDirectoryFullPath = di.FullName;
+
+                foreach (ZipArchiveEntry file in archive.Entries)
+                {
+                    string completeFileName = Path.GetFullPath(Path.Combine(destinationDirectoryFullPath, file.FullName));
+                    string directory = Path.GetDirectoryName(completeFileName);
+
+                    if (!Directory.Exists(directory))
+                    {
+                        Directory.CreateDirectory(directory);
+                    }
+
+                    if (!completeFileName.StartsWith(destinationDirectoryFullPath, StringComparison.OrdinalIgnoreCase))
+                    {
+                        throw new IOException("Trying to extract file outside of destination directory. See this link for more info: https://snyk.io/research/zip-slip-vulnerability");
+                    }
+
+                    
+
+                    if (file.Name == "")
+                    {// Assuming Empty for Directory
+                        Directory.CreateDirectory(Path.GetDirectoryName(completeFileName));
+                        continue;
+                    }
+
+                    file.ExtractToFile(completeFileName, true);
+
+                }
+            }
+
+        public static void DirectoryCopyAndDelete(string strSource, string Copy_dest)
+        {
+            DirectoryInfo dirInfo = new DirectoryInfo(strSource);
+
+            DirectoryInfo[] directories = dirInfo.GetDirectories();
+
+            FileInfo[] files = dirInfo.GetFiles();
+
+            foreach (DirectoryInfo tempdir in directories)
+            {
+
+                Directory.CreateDirectory(Copy_dest + "/" + tempdir.Name);// creating the Directory   
+
+                var ext = System.IO.Path.GetExtension(tempdir.Name);
+
+                if (System.IO.Path.HasExtension(ext))
+                {
+                    foreach (FileInfo tempfile in files)
+                    {
+                        tempfile.CopyTo(Path.Combine(strSource + "/" + tempfile.Name, Copy_dest + "/" + tempfile.Name),true);
+                        File.Delete(tempfile.FullName);
+                    }
+                }
+                DirectoryCopyAndDelete(strSource + "/" + tempdir.Name, Copy_dest + "/" + tempdir.Name);
+            }
+
+            FileInfo[] files1 = dirInfo.GetFiles();
+
+            foreach (FileInfo tempfile in files1)
+            {
+                tempfile.CopyTo(Path.Combine(Copy_dest, tempfile.Name),true);
+                File.Delete(tempfile.FullName);
+
+            }
+            Directory.Delete(dirInfo.FullName);
+        }
+    }
+}
